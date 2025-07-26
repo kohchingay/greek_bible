3b0d55584c68ccc551f9e1f2fa831211d77fb6e7
kohchingay <kohchingay@gmail.com>
2025-07-26 10:37:07 +0800

    Update SBLGNT.py

--- SBLGNT.py
+++ SBLGNT.py
@@ -22,8 +22,10 @@ st.write(f"Number of times {word_nt} appears in the NT is {wordcount_nt}.\n\n")
 
 
 df = pd.DataFrame(
+    {
     "Book": ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"],
-
+    
+    }
 )
 St.table(df)
   
