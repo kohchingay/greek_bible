c4034fadf63a6c76f7666826c4c227b19cb16c75
kohchingay <kohchingay@gmail.com>
2025-07-26 09:58:37 +0800

    Update SBLGNT.py

--- SBLGNT.py
+++ SBLGNT.py
@@ -5,18 +5,19 @@
 
 
 import pandas as pd
+import streamlit as st
 
 f = open("SBLGNT.txt")
 text = f.read()
 
-print("This website refers to the SBL Greek New Testament text available online at sites such as") 
-display(Markdown('[https://gntreader.com/?b=MT&c=1&v=1](https://gntreader.com/?b=MT&c=1&v=1)'))
+st.write("This website refers to the SBL Greek New Testament text available online at sites such as") 
+st.write(Markdown('[https://gntreader.com/?b=MT&c=1&v=1](https://gntreader.com/?b=MT&c=1&v=1)'))
 
 word = input("Copy the word you would like to count from the Greek text (which should be in lower case), \npaste it here and hit the 'Enter' button: \n\n")
 
 wordcount = text.count(word)
 
-display (f"Number of times {word} appears in the NT is {wordcount}.")
+st.write(f"Number of times {word} appears in the NT is {wordcount}.")
 
 
 # In[ ]:
