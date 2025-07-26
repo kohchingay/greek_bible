bf03be789a0bfdf349435bf8a2dc03cb0d2c7cb5
kohchingay <kohchingay@gmail.com>
2025-07-26 20:55:12 +0800

    Update SBLGNT.py

--- SBLGNT.py
+++ SBLGNT.py
@@ -15,7 +15,9 @@ nt_text = sblgnt.read()
 st.write("This website refers to the SBL Greek New Testament text available online at sites such as") 
 st.markdown('[https://gntreader.com/?b=MT&c=1&v=1](https://gntreader.com/?b=MT&c=1&v=1)')
 
-word = st.text_input("Copy the word you would like to count from the Greek text (which should be in lower case), \npaste it here and hit the 'Enter' button: \n\n")
+word = st.text_input("Copy the word you would like to count from the Greek text (which should be in lower case), \npaste it here and hit the 'Enter' button:")
+
+st.markdown("\n\n")
 
 wordcount_nt = nt_text.count(word)
 
