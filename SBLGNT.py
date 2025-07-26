d8820b835111c4233ff03af7dcd721e5eb3b1157
kohchingay <kohchingay@gmail.com>
2025-07-26 20:25:54 +0800

    Update SBLGNT.py

--- SBLGNT.py
+++ SBLGNT.py
@@ -91,8 +91,6 @@ st.dataframe(rounded_df)
 
 import altair as alt
 
-st.title("Word Count")
-
 # Prepare your data
 source = pd.DataFrame({
     "Book": ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"],
